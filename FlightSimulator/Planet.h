0693c4150993347e20820a2f226d5a334262360f
Dayoung Lee <rkek3766@gmail.com>
2020-06-05 22:54:52 +0900

    오브젝트 배치 이제 이 브랜치 버림

---
+++ FlightSimulator/Planet.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "GameObject.h"
+class Planet :
+	public GameObject
+{
+public:
+	Planet();
+	Planet(Planet&);
+	virtual ~Planet();
+
+public:
+	void Init();
+	void Init(float,float _scale = 10.f);
+	virtual bool Frame(float);
+	void Shutdown();
+
+private:
+	void Move(float);
+
+private:
+	float m_fDist;
+	float m_fScale;
+
+};
+
